e753dccc7147003ab67a505dda55f2dcf15c8353
Justin Carpentier <justin.carpentier@inria.fr>
2021-08-22 12:46:25 +0200

    all: enable OSX ARM64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,11 +3,17 @@
 mkdir build
 cd build
 
-cmake .. \
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  export BUILD_NUMPY_INCLUDE_DIRS=$( $PYTHON -c "import numpy; print (numpy.get_include())")
+  export TARGET_NUMPY_INCLUDE_DIRS=$SP_DIR/numpy/core/include
+
+  echo "Copying files from $BUILD_NUMPY_INCLUDE_DIRS to $TARGET_NUMPY_INCLUDE_DIRS"
+  mkdir -p $TARGET_NUMPY_INCLUDE_DIRS
+  cp -r $BUILD_NUMPY_INCLUDE_DIRS/numpy $TARGET_NUMPY_INCLUDE_DIRS
+fi
+
+cmake ${CMAKE_ARGS} .. \
       -DCMAKE_BUILD_TYPE=Release \
-      -DCMAKE_PREFIX_PATH=$PREFIX \
-      -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_INSTALL_LIBDIR=lib \
       -DBUILD_UNIT_TESTS=OFF \
       -DBUILD_BENCHMARK=OFF \
       -DBUILD_EXAMPLES=OFF \
@@ -15,3 +21,10 @@ cmake .. \
 
 make -j${CPU_COUNT}
 make install
+
+if [[ $CONDA_BUILD_CROSS_COMPILATION == 1 ]]; then
+  echo $BUILD_PREFIX
+  echo $PREFIX
+  sed -i.back 's|'"$BUILD_PREFIX"'|'"$PREFIX"'|g' $PREFIX/lib/cmake/crocoddyl/crocoddylTargets.cmake
+  rm $PREFIX/lib/cmake/crocoddyl/crocoddylTargets.cmake.back
+fi
